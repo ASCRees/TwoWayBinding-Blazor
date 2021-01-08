5679bbaa9991b98abbb0588b420b15ff853ceb8e
Andrew Rees <andrew@weareverve.co.uk>
2021-01-08 16:27:57 +0000

    Added additional bunit tests

--- TwoWayBinding-Blazor.Tests/CounterTests.cs
+++ TwoWayBinding-Blazor.Tests/CounterTests.cs
@@ -7,6 +7,8 @@ namespace TwoWayBinding_Blazor.Tests
 {
     public class CounterTests
     {
+        //https://bunit.egilhansen.com/
+
         [Fact]
         public void ParentCounterShouldIncrementWhenClicked()
         {
@@ -36,6 +38,34 @@ namespace TwoWayBinding_Blazor.Tests
             // Assert:  first find the <p> element, then verify its content
             //  cut.Find("#childCounter").MarkupMatches("<p id='childCounter'>Child Current count: 11</p>");
             cut.Find("#childCounter").InnerHtml.Equals("Child Current count: 11");
+        }
+
+        [Fact]
+        public void ParentCounterPassParemeterShouldStartWithFive()
+        {
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<CounterParent>(("CounterStart", "5"));
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Parent Current count: 5</p>");
+        }
+
+        [Fact]
+        public void ParentCounterPassParemeterShouldIncrementWhenClicked()
+        {
+            // Arrange: render the Counter.razor component
+            using var ctx = new TestContext();
+            var cut = ctx.RenderComponent<CounterParent>(("CounterStart", "5"));
+
+            // Act: find and click the <button> element to increment
+            // the counter in the <p> element
+            cut.Find("button").Click();
+
+            // Assert: first find the <p> element, then verify its content
+            cut.Find("p").MarkupMatches("<p>Parent Current count: 6</p>");
         }
     }
 }
---
+++ TwoWayBinding-Blazor/Pages/CounterParent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoWayBinding_Blazor.Pages
+{
+    public partial class CounterParent
+    {
+        [Parameter]
+        public string CounterStart { get; set; }
+
+        private int currentCount = 10;
+
+        protected override void OnInitialized()
+        {
+            if (!string.IsNullOrEmpty(CounterStart))
+            {
+                currentCount = Convert.ToInt32(CounterStart);
+            }
+        }
+
+        private void UpdateCounter(int countValue)
+        {
+            currentCount = countValue;
+        }
+    }
+}
